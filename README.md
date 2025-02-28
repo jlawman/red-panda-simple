a506218f710ba3ecd4e45e2369d5be33ecb1ade3
Josh Lawman <jlawman@gmail.com>
2025-02-28 19:34:21 +0000

    Cleanup

---
+++ README.md
@@ -0,0 +1,5 @@
+#Create a new Red Panda project
+
+```bash
+bash -c 'cd ~/Documents/adder/"100 templates"/red-panda-simple && ./setup/red-panda-creator.sh'
+```
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['utfs.io'],
-  },
-  // ... other configurations
-}
-
-module.exports = nextConfig
